deba09c4f88f48f45f23c4e17602e627889f24e7
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-10-30 00:22:41 -0700

    Create Datatypes.js

---
+++ .devcontainer/Datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) we are using node js not browser
+let name = "raghav"
+let age = 33
+let isLoggedIn = false
+console.table([name,age,isLoggedIn])
+// number => 2 to power 53
+//bigint
+//String => ""
+//boolean
+//null => standalone value\
+//undefined 
+//symbol => unique
+
+
+//object
+console.log(typeof "Raghav")
+console.log(typeof null)
+console.log(typeof undefined)
