9a5080c94d72306977bdc1f0404a3300072bf177
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-10-29 22:59:04 -0700

    var,let,const6

--- .devcontainer/index.js
+++ .devcontainer/index.js
@@ -4,3 +4,4 @@ accountPassword = "9888812"
 accountCity = "pilibhit"
 console.log(accountId)
 console.table([accountId,accountEmail.accountPassword,accountCity])
+// prefer not to use var because of issue in block scope and fuctional scope
