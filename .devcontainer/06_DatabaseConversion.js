4c926099478e80a325cba48abf49c67396523ca5
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-10-30 02:22:54 -0700

    Update 06_DatabaseConversion.js

--- .devcontainer/06_DatabaseConversion.js
+++ .devcontainer/06_DatabaseConversion.js
@@ -4,3 +4,22 @@
 // 1 => true 0 => false
 //"" => false
 //"raghav" => true
+
+/*       *****opertions***   */
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// how to add string in javascript
+// let str1 = "hello"
+// let str2 = " world"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);// string phale  h toh string jaisa behaviour krega
+// console.log(1+ 2 +2);
+// console.log(1 + 2 +"2");//string baad main  h toh phale add hoga phir ja kr string value bnega
+console.log(true);
+console.log(+true);
+console.log(-true);
+console.log(+"");
