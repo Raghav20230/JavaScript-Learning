79a74d6b08de4edd2ef194fe117094ed28977dc5
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-10-30 05:57:18 +0000

    var,let

--- .devcontainer/index.js
+++ .devcontainer/index.js
@@ -1,1 +1,6 @@
-console.log("raghav")
+const accountId = 534342
+accountEmail = "Rahav@gmail.com"
+accountPassword = "9888812"
+accountCity = "pilibhit"
+console.log(accountId)
+console.table([accountId,accountEmail.accountPassword,accountCity])
