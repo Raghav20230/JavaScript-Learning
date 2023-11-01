248cdedbe71d6efb21d9752fd49439cae2359cbe
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-11-01 11:15:55 +0000

    Arrays_01

---
+++ Arrays_01.js
@@ -0,0 +1,16 @@
+const myArr = [0,1,2,3,4]
+// console.log(myArr[0])
+// myArr.push(6)//push any similar number in arrays
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);//basically pop work with last word and clear the last digit with array
+// myArr.unshift(9)// unshift value ko phale add kr deta h 
+// myArr.shift()//first value ko hata deta h output:1,2,3,4 => 0 shift se clear ho jata h 
+/*in java some functions are asking quetions */
+// console.log(myArr.includes(4));
+// console.log(myArr);
+// console.log(myArr.indexOf(3))
+// const newArr = myArr.join()//join ne humre array ko bind kr diya aur convert bhe  kr diya h string main
+// console.log(myArr);
+// console.log(typeof newArr);
+//*slice and splice
