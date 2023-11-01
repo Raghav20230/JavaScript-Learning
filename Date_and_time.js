d724bb8682cc20ab709142fa8e2a434aa734d7a0
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-11-01 09:40:48 +0000

    Date_Time

---
+++ Date_and_time.js
@@ -0,0 +1,18 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.getMonth());
+// console.log(myDate.getUTCHours());
+// console.log(typeof myDate);
+// let myDate = new Date
+// let myCreatedDate = new Date("01-14-2023")
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+console.log(newDate);
