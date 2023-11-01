65241284c79b4f8063a8cf7b4e4969af3e33c399
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-11-01 06:45:07 +0000

    Number&MAths

---
+++ number_maths.js
@@ -0,0 +1,34 @@
+// const score=400
+// console.log(score);
+
+// const balance =new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const othernumber = 234.988
+// console.log(othernumber.toPrecision(1))
+// console.log(othernumber.toPrecision(2))
+// console.log(othernumber.toPrecision(3))
+// console.log(othernumber.toPrecision(4))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+/**************** MATHS ******************/
+// console.log(Math);
+// console.log(Math.abs(-4));// Minus value ko + kr deta h absoulute abs
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(5.5));// its means top wala value ka round off kregga
+// console.log(Math.floor(5.5)); //its means down wala value ka round off kregga
+console.log(Math.random());// 0 and 1 main value dega math.random
+console.log(Math.random()*10) ;
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+const min = 10
+const max = 20
+console.log(math.random() * (max - min +1))
+
