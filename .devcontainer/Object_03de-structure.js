f5c81795ce45bf1ebc13b3d6a11378fc2f3f07c2
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-11-03 23:49:13 -0700

    Create Object_03de-structure.js

---
+++ .devcontainer/Object_03de-structure.js
@@ -0,0 +1,8 @@
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    courseinstructor:"Raghav"
+}
+// course.courseinstructor
+const {courseinstructor:instructor} = course //Object de-structure 
+console.log(instructor);
