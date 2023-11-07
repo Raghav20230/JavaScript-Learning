d9712958996713b3fa2b8b8982fb8784cf2c3524
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-11-07 06:56:41 +0000

    javaScript_Excution_Context.js

---
+++  javascript_execute_code.js
@@ -0,0 +1,26 @@
+//javaScript Excution Context
+/*
+[{}] =>  Global Ec
+           |
+           >this
+Gloabal excution context
+Function excution context
+eval excution context
+*/ 
+// [{}] => memory creation phase
+// excution phase
+
+/*
+1. Global Excution =>this 
+2.Memory phase
+val1 => undefind 
+val2 => undefined
+addnum => defination
+result1 => undefined 
+result2 => undefined
+3.Excution Phase
+val1 => 10
+val2 => 5
+add num => new variable environment 
+                  +
+             Excution Thread      
