ef92fdb56a622c6e735417b9f0c6a10bd124e9af
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-11-08 08:45:47 +0000

    while_Do_While.js

---
+++ While_Do_While.js
@@ -0,0 +1,33 @@
+// let index = 0;
+// while(index <= 10){
+//     console.log(`value of index is ${index}`)
+//     index = index +2
+// }// output=>
+// /usr/local/bin/node ./While_Do_While.js
+// value of index is 0
+// value of index is 2
+// value of index is 4
+// value of index is 6
+// value of index is 8
+// value of index is 10
+
+
+// let myArray = [`Spiderman`,"IronMan","Superman"]
+// let arr = 0
+// while(arr < myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }//output
+///usr/local/bin/node ./While_Do_While.js
+// value is Spiderman
+// value is IronMan
+// value is Superman
+
+// let score = 11
+// do{
+//     console.log(`Score is ${score}`);
+//     score++
+// }while(score <= 10);\
+//Output
+// /usr/local/bin/node ./While_Do_While.js
+// Score is 11
