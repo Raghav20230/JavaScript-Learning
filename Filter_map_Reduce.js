5c02d38d4874030b2518204177ac2f4d585512da
Raghav20230 <147702713+Raghav20230@users.noreply.github.com>
2023-11-09 10:14:03 +0000

    Reduce_Filter_map

---
+++ Filter_map_Reduce.js
@@ -0,0 +1,24 @@
+//const coding =["Note js","java","c","c++","python"] 
+ // const values = coding.forEach((item) => {
+ // console.log(item);
+  //})
+  //console.log(values);//for each kuch bhe return ni krta h
+ // const myNums =[1,2,3,4,5,6,7,8,9,10]
+ //const newNums = myNums.filter( (num) => num > 4 )//without curly  bracket return likhna jaruri ni hai
+ //console.log(newNums);
+
+   //const myNums =[1,2,3,4,5,6,7,8,9,10]
+ //const newNums = myNums.filter( (num) =>{ return num > 4 }) //curly bracket {} lgne pr return likhna jaruri hai
+ //console.log(newNums);//output: [5, 6, 7, 8, 9, 10]
+ 
+// const myNums =[1,2,3,4,5,6,7,8,9,10]
+// const newNums = []
+// myNums .forEach ((num)=> {
+ //if(num > 4){
+ //newNums.push(num)
+ //}
+  //})
+//console.log(newNums);//output:[5, 6, 7, 8, 9, 10] same output by empty array 
+
+    
+    
